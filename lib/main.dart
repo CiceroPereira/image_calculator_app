f160480d4b227824e8c3ddfd0c03be112c25216f
Cicero Pereira de Lima Junior <cjunior3@lenovo.com>
2023-06-25 19:24:56 -0300

    Starting camera access module

--- lib/main.dart
+++ lib/main.dart
@@ -11,11 +11,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Camera Calculator',
       theme: ThemeData(
-        primarySwatch: Color.fromARGB(255, 45, 0, 53),
+        primarySwatch: Colors.cyan,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Camera Calculator'),
     );
   }
 }
@@ -30,14 +30,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -50,17 +43,13 @@ class _MyHomePageState extends State<MyHomePage> {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Click on camera button to start calculating!',
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed:() => print("tapped"),
         tooltip: 'Increment',
         child: const Icon(Icons.camera_alt_outlined),
       ), 
